6e3e7454f001acb1bcef461ba8606d76e93b6e65
B.Martin <96883665+Benja-Martin@users.noreply.github.com>
2023-11-24 13:17:23 +0100

    Simulations.py

--- Simulations.py
+++ Simulations.py
@@ -1,10 +1,16 @@
 """
-Script Python permettant de faire des simulations multiples sur 
-une eprouvette de type AE2 dans le SBR caracterise, de projecter 
-les invariants K2 et K3 sur les noeuds et d'extraire et d'ecrire 
-les donnees qui nous interessent. 
+has to be executed by Abaqus' internal interpreter, to automate simulations and extract data.
+This script breaks down into three steps and requires modification by the user. The first step is to run all the
+simulations. The second step is to project the calculated values for the Gauss point invariants onto the mesh nodes.
+The third step is to extract the data at the node of interest specified by the user. The modifications to be made
+by the user are minor: the maximum and minimum values of vertical displacements and torsion angles, and their
+associated steps, the path to the .cae file, the name of the initial job, the name of the .odb files to be created, the
+name of the specimen, the IDs of the nodes from which the values of macroscopic stresses and invariants will be
+extracted, the location of the .odb files and the name of the .csv file where the data will be saved. The user can also
+choose which steps will be performed.
+WARNING : some versions of Abaqus may not support special or accented characters 
 """
-### Import des modules 
+### Importing modules
 from abaqus import *
 from abaqusConstants import *
 from caeModules import *
@@ -15,67 +21,71 @@ executeOnCaeStartup()
 print('Importation des modules : OK')
 
 #####################################################################
+#####################################################################
+#####################################################################
 
-### Donnees des simulations
-    # Deplacemements qui seront imposes
+### Simulation step 
+    # Vertical displacement min, max and associated step
 U_min = -2
 U_max = 10
 U_pas = 0.1
-    # Torsion qui seront imposees
+    # Torsion min, max and associated step
 T_min = np.deg2rad(0)
 T_max = np.deg2rad(100)
 T_pas = np.deg2rad(1)
-    # Chemin vers le fichier .cae
-path_cae = r'D:\Comparaison LDC\ArrudaBoyce\L1_M01\AE2_AB_L1_M01.cae'
-    # Nom du job initial
-init_job = 'Job-1'
-simu_job = 'Job-AE2_L1_M01_'
+    # Path for the .cae file
+path_cae = XXXX
+    # Initial job name
+init_job = XXXX
+    # Current job name
+simu_job = XXXX
 
-### Projection K2 et K3
-    # Nom de l'eprouvette
-nom_eprouvette = 'AE2_L1_M01_'
-	# Noeud en surface de la section nominale et element associe
-node_surf_label = 1
-element_surf_label = 1
-	# Noeud sur la surface superieure et element associe
-node_input_label = 11853
-element_input_label = 3848
-    # Repertoire ou sont les .odb
-monRepertoire = r'D:\Comparaison LDC\ArrudaBoyce\L1_M01\Simulations'
+### Projection step
+    # Sample name
+nom_eprouvette = XXXX
+	# Node ID and associated element ID for invariants extraction
+node_surf_label = XXXX
+element_surf_label = XXXX
+	# Node ID and associated element ID for inputs extraction
+node_input_label = XXXX
+element_input_label = XXXX
+    # Repository for .odb file 
+monRepertoire = XXXX
 
-### Ecriture des donnees
-    # Fichier ou les donnees seront sauvegardees
-data_save_file = r'D:\Comparaison LDC\ArrudaBoyce\L1_M01\carte_'+nom_eprouvette+'.csv'
+### Data writing step
+    # File where data will be saved
+data_save_file = XXXX +'\carte_'+nom_eprouvette+'.csv'
 
 ### Etapes a faire
     # Simulations
-simu_statut = False
+simu_statut = True
     # Projection
-projection_statut = False
+projection_statut = True
     # Ecriture
 ecriture_statut = True
 
-
+#####################################################################
+#####################################################################
 #####################################################################
 
 if simu_statut:
-    ### Simulations mutliples
-        # Construction des listes des valeurs de deplacement et de torsion
+    ### Useful variables
+        # Creating lists of displacement and torsion values
     U_liste = np.arange(U_min, U_max+U_pas/2, U_pas)
     T_liste = np.arange(T_min, T_max+T_pas/2, T_pas)
-        # Nombre de simulation
+        # Number of simulations
     N = len(T_liste)
-        # Ouverture du fichier CAE
+        # Opening .cae file
     openMdb(pathName=path_cae)    
-    print('Ouverture du fichier '+path_cae+' : OK')
-        # Reinitialisation des conditions initiales a U2=0 et UR2=0
+    print('File opening '+path_cae+' : OK')
+        # Reset initial conditions to U2=0 and UR2=0
     mdb.models['Model-1'].boundaryConditions['Deplacement'].setValues(amplitude=UNSET, u2=0)
     mdb.models['Model-1'].boundaryConditions['precharge'].setValues(amplitude=UNSET, ur2=0)
-        # Modification de l'increment de temps
+        # Time increment modification 
     time_increment = np.round( U_pas/(U_max-U_min) , 3)
     mdb.models['Model-1'].steps['Step-1'].setValues(initialInc=time_increment, minInc=10**-5, maxInc=time_increment)
-    
-        # Enchainement des simulations
+    print('Simulations to do : '+str(N))
+        # Running simulations
     i = 0
     for t in T_liste:
         mdb.models['Model-1'].boundaryConditions['precharge'].setValues(amplitude=UNSET, ur2=t, u2=U_min)
@@ -88,20 +98,22 @@ if simu_statut:
         print('Simulations : ' + str(i) + '/' + str(N))
         
 #####################################################################
+#####################################################################
+#####################################################################
 
 if projection_statut:
-    ### Recuperation des fichiers .odb
-        # Initialisation de la liste des fichiers .odb
+    ### Recovering .odb files
+        # Initializing the list of .odb files
     fichiers = []
-        # Parcours de tous les fichiersdu repertoire
+        # Browse all directory files
     for file in os.listdir(monRepertoire):
-        # Verification si le fichier se termine par '.odb'
+        # Check if the file ends with '.odb'.
         if file.endswith('.odb'):
-            # Ajout du fichier a la liste des fichiers .odb
+            # Add file to list of .odb files
             fichiers.append(os.path.join(monRepertoire, file))
     N = len(fichiers)
 
-    ### Creation des subfield K2 et K3 a partir des UVARM2 et UVARM3
+    ### Creation of subfields K2 and K3 from UVARM2 and UVARM3
     error = []
     for i,f in enumerate(fichiers):
         try:
@@ -110,14 +122,14 @@ if projection_statut:
             for k in range(len(step.frames)):
                 frame = step.frames[k]
                 try:
-                        # Subfield K2 a partir de UVARM2
+                        # Subfield K2 from UVARM2
                     sourceField=frame.fieldOutputs['UVARM2']
                     subField=sourceField.getSubset(position=ELEMENT_NODAL)
                     newField=frame.FieldOutput(name='K2', field=subField)
                 except:
                     pass
                 try:
-                        # Subfield K3 a partir de UVARM3
+                        # Subfield K3 from UVARM3
                     sourceField=frame.fieldOutputs['UVARM3']
                     subField=sourceField.getSubset(position=ELEMENT_NODAL)
                     newField=frame.FieldOutput(name='K3', field=subField)
@@ -127,29 +139,31 @@ if projection_statut:
             odb.close()
         except:
             error.append(f)
-        # Avancement 
+        # Progress 
         print('Projection de K2 et K3 sur les noeuds : '+ str(i+1) + '/' + str(N))
     print('Projections finies : il y a eu '+str(len(error))+' erreur(s)')
     print(error)
         
 #####################################################################
+#####################################################################
+#####################################################################
 
 if ecriture_statut:
-    ### Recuperation des fichiers .odb
+    ### Recovering .odb files
         # Initialisation de la liste des fichiers .odb
     fichiers = []
-        # Parcours de tous les fichiersdu repertoire
+        # Browse all directory files
     for file in os.listdir(monRepertoire):
-        # Verification si le fichier se termine par '.odb'
+        # Check if the file ends with '.odb'.
         if file.endswith('.odb'):
-            # Ajout du fichier a la liste des fichiers .odb
+            # Add file to list of .odb files
             fichiers.append(os.path.join(monRepertoire, file))
     N = len(fichiers)
-    ### Recuperation des donnees
-        # Ouverture/Creation du fichier dans lequel seront sauvegarde les resultats
+    ### Data recovery
+        # Opening/Creating the file in which results will be saved
     H = open(data_save_file, 'a')
     H.write('U,T,K2,K3\n')
-        # Recuperation des indices ou il faut recuperer les donnees 
+        # Recuperation of indexes where data must be recovered 
     ff = session.openOdb(fichiers[0])
     step  = ff.steps['Step-1']
     K2  = ff.steps['Step-1'].frames[0].fieldOutputs['K2']
@@ -164,7 +178,7 @@ if ecriture_statut:
             if K2.values[i].nodeLabel==node_surf_label:
                 indice_donnees = i
     ff.close()
-        # Recuperation des donnees
+        # Data recovery
     N_erreur_ecriture = 0
     for count, f in enumerate(fichiers):
         try:
@@ -177,14 +191,16 @@ if ecriture_statut:
                 K3  = ff.steps['Step-1'].frames[k].fieldOutputs['K3'].values[indice_donnees].data
                 H.write(str(U2)+','+str(UR2)+','+str(K2)+','+str(K3)+'\n')
             ff.close()
-            print('Ecriture des donnees : ' + str(count+1) + '/' + str(len(fichiers)))
+            print('Data writing : ' + str(count+1) + '/' + str(len(fichiers)))
         except:
             N_erreur_ecriture += 1
             print('Erreur')
     H.close()
-    print('Ecriture des donnees : '+str(N_erreur_ecriture)+' erreur(s)')
+    print('Data writing : '+str(N_erreur_ecriture)+' error(s)')
 
-############################
+#####################################################################
+#####################################################################
+#####################################################################
 
 print(' ')
-print('FINI')
+print('THE END')
